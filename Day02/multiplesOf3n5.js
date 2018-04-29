14a8bf951859c446c0771744ef5ce492cd209fe7
Josh Bedo <joshbedo@yahoo.com>
2018-04-29 05:26:19 -0400

    find the sum of all multiples of 3 or 5 below 1000

---
+++ Day02/multiplesOf3n5.js
@@ -0,0 +1,25 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3,5,6, 9. the sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000
+ */
+
+const add = (a,b) => {
+  return a+b;
+}
+
+const findSum = n => {
+  let arr = [];
+
+  for (var i = 0; i < n; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      arr.push(i);
+    }
+  }
+
+  // Return the sum
+  return arr.reduce(add, 0);
+}
+
+findSum(1000);
